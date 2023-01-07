f3f292c015e4bd03794c6b0527fce397fff9ee02
techtorialdevopsb2 <yusufcanturan@icloud.com>
2023-01-07 11:14:56 -0600

    Simple Math and hello.py is committed

---
+++ SimpleMath.py
@@ -0,0 +1,38 @@
+
+# In the lines below we are going to print number and the 
+# text in the same line seperated by comma.
+
+#When we use comma in the print statement it adds
+# space after the first variable.
+
+print("8 times 7 is",7 * 8)
+
+print("8 minus 7 is",8 - 7)
+
+print("8 plus 7 is",8 + 7)
+
+print("8 divided by 7 is",8 / 7)
+
+
+#Q: What will be the output of the print statement below? 
+print("8*7")
+#A -> Syntax Error
+#B -> 56
+#C -> 8*7//
+
+# Everything in single or double quotation is called string 
+# in python.
+
+
+
+print("""Hello""")
+
+
+
+
+
+
+
+
+
+
---
+++ hello.py
@@ -0,0 +1,40 @@
+
+# Note: Line below prints the text with double quotation
+print("Hello World")
+
+print('Hello World')
+
+# Highlight the parts you want to comment and 
+# press command(ctrl) + /
+
+# What if I want to print multiple lines with 
+# one print function ? 
+# We have to use triple double quotation or 
+# triple single quotation 
+print ("""Hello, 
+This is the example for 
+multiline in single print function""")
+
+print('''Hello,
+This is the example for multiline
+in single print function using triple single quotation''')
+
+
+# I'd like to watch a movie
+
+print ("I'd like to watch a movie")
+
+
+# I'm fond of the saying "something"
+
+# When we define the sentence above in a python text
+# we should let python know which quotations it should not consider
+# as a python code
+#Way to tell python it is in the text is using \ (backslash)
+print(" I'm fond of the saying \"something\" ")
+
+#How do I define the sentence in a single quotated text? 
+print(' I\'m fond of the saying "something" ')
+
+
+
